bdfe6a15e459c1dd3b696314ab8de54a115dd62c
Mike Cardwell <mcardwell@centiq.co.uk>
2015-11-30 12:11:39 +0000

    Base package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'reactable',
+  version: '0.0.1',
+  summary: 'A library for displaying reactive data in a HTML table',
+  git: 'https://github.com/Centiq/meteor-reactable',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+
+  api.use([
+    'react',
+    'react-meteor-data',
+  ]);
+
+});
